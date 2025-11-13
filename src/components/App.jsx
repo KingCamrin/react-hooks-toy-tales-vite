74df46ad0d48067e16a7265f7eaa895bf1290303
Camrin <camrindavis58@gmail.com>
2025-11-13 11:41:23 -0600

    Fixed the imlementation of toycard,form,container, to get accepted into app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,21 +6,73 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function handleAddToy(newToy) {
+    setToys([...toys, newToy]);
+  }
+
+function handleLike(toy) {
+  const newLikes = toy.likes + 1;
+
+  fetch(`/toys/${toy.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ likes: newLikes }),
+  })
+    .then((res) => res.json())
+    .then((updatedToy) => {
+      setToys(toys.map((t) =>
+        t.id === updatedToy.id ? updatedToy : t
+      ));
+    });
+}
+function handleDelete(toy) {
+fetch(`/toys/${toy.id}`, {
+  method: "DELETE"
+})
+.then(() => {
+  setToys(toys.filter(t => t.id !== toy.id));
+});
+}
+useEffect(() => {
+  fetch(`http://localhost:3001/toys`)
+  .then((res) => res.json())
+  .then((toyData) => {
+    setToys(toyData)
+     });
+     }, []);
+
+
+
+
+
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddToy={handleAddToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} 
+      onLike={handleLike} 
+      onDelete={handleDelete}/>
     </>
   );
 }
 
+
+
 export default App;
+
+
+
+
+
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,23 +1,59 @@
 import React from "react";
+import {useState} from 'react'
 
-function ToyForm() {
+
+function ToyForm({ onAddToy}) {
+
+  const [name, setName] = useState("");
+  const[image, setImage] = useState("");
+
+  function handleSubmit(e) {
+  e.preventDefault();
+
+  const newToy = {
+    name: name,
+    image: image,
+    likes: 0
+  };
+
+  fetch("http://localhost:3001/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newToy),
+  })
+    .then((res) => res.json())
+    .then((toy) => {
+      onAddToy(toy);
+      setName("");
+      setImage("");
+    });
+}
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
-        <input
-          type="text"
-          name="name"
-          placeholder="Enter a toy's name..."
-          className="input-text"
-        />
+    <input
+  type="text"
+  name="name"
+  value={name}
+  onChange={(e) => setName(e.target.value)}
+  placeholder="Enter a toy's name..."
+  className="input-text"
+/>
+
         <br />
-        <input
-          type="text"
-          name="image"
-          placeholder="Enter a toy's image URL..."
-          className="input-text"
-        />
+     <input
+  type="text"
+  name="image"
+  value={image}
+  onChange={(e) => setImage(e.target.value)}
+  placeholder="Enter a toy's image URL..."
+  className="input-text"
+/>
+
         <br />
         <input
           type="submit"
@@ -31,3 +67,6 @@ function ToyForm() {
 }
 
 export default ToyForm;
+
+
+
